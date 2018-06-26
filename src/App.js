5dc371bd94a10b0ed23e09a7aa2cf7f71dbb01a4
Milton Bittencourt <miltonbst@gmail.com>
2018-06-25 23:30:11 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Veg Reviews</h1>
+        </header>
+        <p className="App-intro">
+         Dedicado a veganinha mais linda de mundo, Lili
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
